8db6f869e05424b7fbd58a80018086e7d78f3d35
Aaron Fujimoto <aaronfuj@gmail.com>
2020-07-31 22:58:17 -1000

    Added/updated the fields with real information

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,7 +2,7 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
+  title: 'Aaron Fujimoto', // e.g: 'Name | Developer'
   lang: '', // e.g: en, es, fr, jp
   description: '', // e.g: Welcome to my website
 };
@@ -10,18 +10,22 @@ export const headData = {
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  name: 'Aaron Fujimoto',
+  subtitle: ' ',
+  cta: 'Get to know me',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne:
+    'Hello! My name is Aaron Fujimoto and I have had several Software Engineering related titles over the years. ' +
+    'Ultimately, I enjoy using technology to solve problems.',
+  paragraphTwo:
+    'My experiences range from designing large scale on-premise systems to User Experience in Mobile/Web Apps and everywhere in between.',
+  paragraphThree:
+    'Away from the keyboard, I enjoy skimboarding and cooking. I have two cats. Yes, that is important.',
+  resume: '', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
@@ -29,29 +33,30 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
-    info: '',
+    title: 'SurfNewsNetwork (Android App)',
+    info:
+      'An Android App for Hawaii Surf Reports, Forecasts, and Tide Charts to allow less time on your phone and more time at the beach',
     info2: '',
-    url: '',
+    url: 'https://play.google.com/store/apps/details?id=com.fujimoto.hsf',
+    repo: '', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: 'Hawaii Power Ballot',
+    info: 'A robust guide to who and what will be on the 2018 General Election ballot',
+    info2: '',
+    url: 'http://www.hawaiipowerballot.com/',
     repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
-    info: '',
+    title: 'Contest Scoreboard',
+    info: 'A real-time, live scoring software to replace traditional pen and paper for contests',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://contest-scoreboard.herokuapp.com/',
+    repo: 'https://github.com/aaronfuj/contest_scoreboard', // if no repo, the button will not show up
   },
 ];
 
@@ -59,7 +64,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'aaronfuj@gmail.com',
 };
 
 // FOOTER DATA
@@ -67,28 +72,23 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
+      name: 'envelope',
+      url: 'mailto:aaronfuj@gmail.com',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/aaronfujimoto/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/aaronfuj',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
