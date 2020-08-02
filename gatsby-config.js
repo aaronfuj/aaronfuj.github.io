23526bede7eb987c6ac8b788f66b0e4912a8f94a
Aaron Fujimoto <aaronfuj@gmail.com>
2020-08-01 14:23:44 -1000

    Updated gatsby config, added CNAME

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        name: `Aaron Fujimoto`,
+        short_name: `Aaron Fujimoto`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
